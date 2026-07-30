00001261390573a21af58cd71b4d9fdc1c8a98a6
DRD <Saxena0794@gmail.com>
2026-07-30 12:51:33 +0530

    changes CandidateDetailsAdapter        private fun decodeFollowUpStatus(status: String): String = when (status) { //            "1" -> "Settlement In Progress"   rmove this status as per discussion with Gaurav dutta 27-05-2026             "1" -> "Unsettled"             "2" -> "Settled"
                else -> "N/A"
            }
            we have check on Demo so issues has been resolved but not  check  live  server backend

--- app/src/main/java/com/rsetiapp/common/adapter/CandidateDetailsAdapter.kt
+++ app/src/main/java/com/rsetiapp/common/adapter/CandidateDetailsAdapter.kt
@@ -115,9 +115,9 @@ class CandidateDetailsAdapter(
 
         private fun decodeFollowUpStatus(status: String): String = when (status) {
 //            "1" -> "Settlement In Progress"   rmove this status as per discussion with Gaurav dutta 27-05-2026
-            "1" -> "Settlement In Progress"
-            "1" -> "Settled"
-            "2" -> "Unsettled"
+            "1" -> "Unsettled"
+            "2" -> "Settled"
+
             else -> "N/A"
         }
 
